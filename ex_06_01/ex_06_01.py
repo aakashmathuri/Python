8a9360a5ea1b4a14430c5cac278b34f450419511
Aakash <aakash1350@gmail.com>
2020-07-18 20:35:51 +0530

    Added a book By Dr. Chuck

--- ex_06_01/ex_06_01.py
+++ ex_06_01/ex_06_01.py
@@ -4,9 +4,3 @@ while index < len(fruit):
     letter = fruit[index]
     print(letter)
     index = index - 1
-word = 'Jajantaram'
-count = 0
-for letter in word:
-    if letter == 'a':
-        count += 1
-print(count)
