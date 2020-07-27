e3bd9aecd648471ff9399ed40c1513618cce6f57
Aakash <aakash1350@gmail.com>
2020-07-27 07:59:35 +0530

    .

--- ex_07_00/ex_07_03.py
+++ ex_07_00/ex_07_03.py
@@ -1,6 +1,6 @@
 fhand = open('mbox-short.txt')
-count = 0
+
 for line in fhand:
     line = line.rstrip()        # rstrip() method remove all whitespace.
-    if line.startswith('From'): # startswith() method is used for whatever word or characters we want to print from.
+    if line.startswith('From:'): # startswith() method is used for whatever word or characters we want to print from.
         print(line)
