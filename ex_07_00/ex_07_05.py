aa49ac1e51dd10a92c4be8b1116ded9f65d4c9bb
Aakash <aakash1350@gmail.com>
2020-07-27 08:57:06 +0530

    file handling

--- ex_07_00/ex_07_05.py
+++ ex_07_00/ex_07_05.py
@@ -1,6 +1,7 @@
 fhand = open('mbox-short.txt')
 for line in fhand:
     line = line.rstrip()
-    if line.find('@uct.ac.za') == -1: continue
+    if line.find('@uct.ac.za') == -1:
+        continue
     # find() method is used for find  from the given characters.
     print(line)
